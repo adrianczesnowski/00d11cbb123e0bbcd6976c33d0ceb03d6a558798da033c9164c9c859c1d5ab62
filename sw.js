ff4c0522b22e36020899f3098888d722b71ea785
Adrian Czesnowski <adrian@MacBook-Air-Adrian.local>
2025-10-12 20:14:56 +0200

    fix

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const CACHE_NAME = 'secure-notes-v1';
 const OFFLINE_URL = '/index.html';
-const STATIC_ASSETS = ['/', '/index.html', '/css/style.css', '/js/app.js', '/js/db.js', '/js/auth.js', '/js/ocr.js', '/js/speech.js', '/assets/icon-192.png', '/assets/icon-512.png'];
+const STATIC_ASSETS = ['/', './index.html', './css/style.css', './js/app.js', './js/db.js', './js/auth.js', './js/ocr.js', './js/speech.js', './assets/icon-192.png', './assets/icon-512.png'];
 self.addEventListener('install', e => {
     e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_ASSETS)));
     self.skipWaiting();
